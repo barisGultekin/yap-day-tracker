7c849932371780f8958864349ddbf24fdfeca710
Ali Baris Gultekin <alibarisgultekin@gmail.com>
2021-04-27 12:58:57 +0300

    Add tableView methods.

--- Yap/TodoListViewController.swift
+++ Yap/TodoListViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class TodoListViewController: UITableViewController
 {
+    let itemArray =
+        [
+            "Find a car.",
+            "Leave the town",
+            "Climb a mountain"
+        ]
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    
+    //MARK: - TV Data Source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView
+            .dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - TV Interaction Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
