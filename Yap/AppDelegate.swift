8fc9e58290119357a3bb991f5f2460fdb6251dc0
Ali Baris Gultekin <alibarisgultekin@gmail.com>
2021-05-12 00:39:10 +0300

    Add CocoaPods and Mongo DB Realm

--- Yap/AppDelegate.swift
+++ Yap/AppDelegate.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+                
+        do
+        {
+            let realm = try Realm()
+        }
+        catch
+        {
+            print("Error initializin Realm: \(error)")
+        }
+        
         return true
     }
 
